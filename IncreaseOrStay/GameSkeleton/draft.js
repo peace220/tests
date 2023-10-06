3fab4f0f1c67f2f9b210030553659e542b805005
Peace220 <jingzhisim0123@gmail.com>
2023-10-06 09:48:48 +0800

    Calculating House Edge

--- IncreaseOrStay/GameSkeleton/draft.js
+++ IncreaseOrStay/GameSkeleton/draft.js
@@ -1,36 +1,58 @@
 function HouseEdge() {
-    const initialBet = 1;
-    let multiplier = 0.6;
-    let loseMultiplier = 0.3;
-    let ev = 0
+    let initialBet = 1; // dollar
+    let multiplier = 1;
+    let loseMultiplier = 0;
+    let expectedvalue = 0 //dollar/outcome == 1
     let wincon = 0.5
     let losecon = 0.5;
     let payout = 0;
     let reward = 1;
     for (i = 0; i < 5; i++) {
         payout = initialBet * multiplier;
-
-        let win = (0.5 * (reward + payout))
+        let win = (0.5 * (reward + payout)) // payout= 0.6 reward = previous win;
         let lost = (0.5 * (reward * loseMultiplier));
         reward += payout;
-        ev = win + lost;
+        expectedvalue = win + lost; // all possible outcome
         if (losecon <= 0.9) {
             losecon += 0.1;
         }
         if (wincon >= 0.2) {
             wincon -= 0.1;
         }
-        const returnToCasino = initialBet - ev;
+
+        const returnToCasino = initialBet - expectedvalue;
+
         const houseEdge = (returnToCasino / initialBet) * 100;
-        console.log("expected Value= ",ev);
-        console.log("Reward =", reward);
-        console.log("Win =",win)
-        console.log("lost =",lost)
+        console.log("inital bet =", initialBet, "Payout =", reward);
         console.log("house Edge = ", houseEdge)
         console.log("");
+        initialBet = reward;
     }
-
-
 }
 
-HouseEdge()
+
+function DoubleOrNothing() {
+    let Chance = Math.floor((Math.random() * 100) + 1);
+    let initialBet = 1;
+    let multiplier = 2;
+    let wincon = 50;
+    console.log("User Inital Bet:", initialBet)
+    for (i = 0; i < 5; i++) {
+        if (Chance <= wincon) {
+            Chance=Math.floor((Math.random() * 100) + 1);
+            initialBet = initialBet * multiplier;
+
+        } else {
+            return console.log("User lost Bet at: ", initialBet,"       Lost!, Output Reward=0")
+        }
+    }
+    return console.log("User Bet: ", initialBet,"       Win!, Output Reward= ", initialBet);
+}
+
+
+for (j = 0; j < 100; j++) {  
+    DoubleOrNothing()
+    console.log("");
+}
+
+// first bet 2 output, 2nd bet 3 output, 3rd bet 4 output
--- IncreaseOrStay/GameSkeleton/draft2.js
+++ IncreaseOrStay/GameSkeleton/draft2.js
@@ -5,26 +5,34 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-const INITIAL_BET = 1;
+let totalWins = 0;
+let totalLosses = 0;
+let totalReward = 0;
+let totalGame = 0;
+const INITIAL_BET = 100;
+const TOTAL_SIMULATIONS = 1000;
+let totalHouseEdge = 0;
+let totalBets = 0;
+let houseEdge = 0
 function getReward(round) {
     const randomValue = Math.random();
-    
+
     let rewardMultiplier;
     switch (round) {
         case 1:
-            if (randomValue < 0.4) return 0;
+            if (randomValue < 0.5) return 0;
             else if (randomValue < 0.8) rewardMultiplier = (Math.random() * 1) + 0.5; // 50% to 150%
             else rewardMultiplier = (Math.random() * 1) + 2; // 200% to 300%
             break;
 
         case 2:
-            if (randomValue < 0.4) return 0;
+            if (randomValue < 0.5) return 0;
             else if (randomValue < 0.8) rewardMultiplier = (Math.random() * 1) + 0.5; // 50% to 150%
             else rewardMultiplier = (Math.random() * 3) + 2; // 200% to 500%
             break;
 
         case 3:
-            if (randomValue < 0.4) return 0;
+            if (randomValue < 0.5) return 0;
             else if (randomValue < 0.8) rewardMultiplier = (Math.random() * 1) + 0.5; // 50% to 150%
             else rewardMultiplier = (Math.random() * 8) + 2; // 200% to 1000%
             break;
@@ -33,9 +41,37 @@ function getReward(round) {
             console.log("Invalid round");
             return 0;
     }
-    
+
     return parseFloat((INITIAL_BET * rewardMultiplier).toFixed(2));
 }
+
+function playgame(round = 1) {
+    const reward = getReward(round);
+    totalReward += reward;
+    totalBets += INITIAL_BET;
+
+    totalGame = totalWins + totalLosses;
+    if (reward === 0 || round === 3) {
+        if (reward === 0) {
+            totalLosses++;
+        }
+        else {
+            totalWins++;
+        }
+
+        if (totalGame < TOTAL_SIMULATIONS) {
+            playgame();
+        } else {
+            houseEdge = (totalBets - totalReward) / totalBets;
+            // console.log(`House Edge: ${(houseEdge * 100).toFixed(2)}%`);
+            rl.close();
+        }
+    } else {
+        playgame(round + 1);
+    }
+}
+
+
 
 function playGame(round = 1, totalReward = 0) {
     const reward = getReward(round);
@@ -59,11 +95,17 @@ function playGame(round = 1, totalReward = 0) {
             rl.close();
         }
     });
-    
+
 }
 
-console.log("Welcome to the betting game!");
-playGame();
+for (iteration = 0; iteration < 100000; iteration++) {
+    playgame()
+    totalHouseEdge += houseEdge;
+}
+const averageHouseEdge = totalHouseEdge / 100000;
+console.log(averageHouseEdge * 100);
+
+
 
 // to do, so in round2 and round 3, house edge is negative. i leave it to you to tune the house edge.
 // and this is a simple draft. please improve upon it, make it more exciting.
